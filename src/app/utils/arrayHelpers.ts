05de9d651230541331d463864a9634350834b36c
Don Denton <don@happycollision.com>
2017-12-04 18:31:00 -0600

    clean up array helpers

--- src/app/utils/arrayHelpers.ts
+++ src/app/utils/arrayHelpers.ts
@@ -1,71 +1,80 @@
-export function remove(item: Object, array: Object[]) {
+export function remove(item: any, array: any[]) {
   var index = array.indexOf(item);
-  if (index >= 0)
-  array.splice(index, 1);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
 }
 
-export function add(item: Object, array: Object[]) {
+export function removeAtIndex(index: number, array: any[]) {
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+}
+
+export function add(item: any, array: any[]) {
   array.push(item);
 }
 
-export function clear(array: Object[]) {
+export function clear(array: any[]) {
   array.splice(0, array.length);
 }
 
-export function insertAfter(index: number, item: Object, array: Object[]) {
-  if ((index >= 0) && (index < array.length))
+export function insertAfter(index: number, item: any, array: any[]) {
+  if ((index > -1) && (index < array.length))
   {
     array.splice(index, 0, item);
   }
 }
 
-export function insertBefore(index: number, item: Object, array: Object[]) {
+export function insertBefore(index: number, item: any, array: any[]) {
   if ((index >= 1) && (index < array.length)) {
     array.splice(index-1, 0, item);
   }
 }
 
-export function anyOfFirstIsInSecond(firstArray: Object[], secondArray: Object[]) {
+export function anyOfFirstIsInSecond(firstArray: any[], secondArray: any[]) {
   for (var i = 0; i < firstArray.length; i++){
-    if (secondArray.indexOf(firstArray[i]) > -1)
-    return true;
+    if (secondArray.indexOf(firstArray[i]) > -1) {
+      return true;
+    }
   }
 
   return false;
 }
 
-export function allOfFirstIsInSecond(firstArray: Object[], secondArray: Object[]) {
+export function allOfFirstIsInSecond(firstArray: any[], secondArray: any[]) {
   for (var i = 0; i < firstArray.length; i++) {
-    if (secondArray.indexOf(firstArray[i]) == -1)
-    return false;
+    if (secondArray.indexOf(firstArray[i]) == -1) {
+      return false;
+    }
   }
 
   return true;
 }
 
-export function canMoveUp(item:Object, array:Object[]):boolean{
+export function canMoveUp(item: any, array: any[]): boolean{
   return (array.indexOf(item) > 0);
 }
 
-export function canMoveDown(item:Object, array:Object[]):boolean{
+export function canMoveDown(item: any, array: any[]): boolean{
   return (array.indexOf(item) < array.length - 1);
 }
 
-export function moveUp(item:Object, array:Object[]){
+export function moveUp(item: any, array: any[]){
   if(this.canMoveUp(item, array)){
     var index = array.indexOf(item);
     this.delete(item, array);
-    this.insertAfter(index-1, item, array);
+    this.insertAfter(index - 1, item, array);
   }
 }
 
-export function moveDown(item:Object, array:Object[]){
+export function moveDown(item: any, array: any[]){
   if(this.canMoveDown(item, array)){
     var index = array.indexOf(item);
     this.delete(item, array);
     if (index == array.length - 1)
     array.push(item);
     else
-    this.insertAfter(index+1, item, array);
+    this.insertAfter(index + 1, item, array);
   }
 }
