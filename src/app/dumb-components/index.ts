3de54fc527d52180ff38f8dea6ac983328a31809
Don Denton <don@happycollision.com>
2018-01-18 13:03:07 -0600

    Add package to publish

---
+++ src/app/dumb-components/index.ts
@@ -0,0 +1,4 @@
+export * from './animated-ellipsis/animated-ellipsis.component';
+export * from './collapsible-panel/collapsible-panel.component';
+export * from './modal/modal.component';
+export * from './sortable-container/sortable-container.component';
