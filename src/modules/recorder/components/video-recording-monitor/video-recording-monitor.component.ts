d98f0217b0a060db070d12bc727d029f8c9be0b3
Don Denton <don@happycollision.com>
2018-06-25 14:21:52 -0500

    add video-recording-monitor

---
+++ src/modules/recorder/components/video-recording-monitor/video-recording-monitor.component.ts
@@ -0,0 +1,80 @@
+import { Component, ViewChild, AfterViewInit, OnDestroy, isDevMode } from '@angular/core';
+import { RecorderService } from '../../recorder.service';
+import { SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-record-rtc',
+  templateUrl: './record-rtc.component.html',
+  styleUrls: ['./record-rtc.component.scss'],
+})
+export class RecordRTCComponent implements AfterViewInit, OnDestroy {
+
+  private stream: MediaStream;
+
+  @ViewChild('video') _video;
+  get video(): HTMLVideoElement {
+    return this._video.nativeElement;
+  }
+
+  constructor(
+    private recorder: RecorderService,
+  ) { }
+
+  ngAfterViewInit() {
+    this.setup();
+  }
+
+  async setup() {
+    const stream = await this.recorder.setup();
+    if (stream) {
+      this.stream = stream;
+      this.setPlayerToMonitorMode();
+    }
+  }
+
+  ngOnDestroy() {
+    this.killStream();
+  }
+
+  setPlayerToMonitorMode() {
+    Object.assign(this.video, {
+      muted: true,
+      controls: false,
+      autoplay: true,
+      src: null,
+      srcObject: this.stream,
+    });
+  }
+
+  setPlayerToReviewMode(url: string | SafeUrl) {
+    Object.assign(this.video, {
+      muted: false,
+      controls: true,
+      autoplay: false,
+      srcObject: null,
+      src: url,
+    });
+  }
+
+  reset() {
+    this.setPlayerToMonitorMode();
+  }
+
+  startRecording() {
+    this.setPlayerToMonitorMode();
+    this.recorder.startRecording();
+  }
+
+  async stopRecording() {
+    await this.recorder.stopRecording();
+    this.setPlayerToReviewMode(this.recorder.lastSuccessfulRecording.url);
+  }
+
+  killStream() {
+    this.recorder.killStream();
+  }
+
+  download() {
+    this.recorder.download();
+  }
+}
