9c80cbed7055ab22c0688e74ebe1ee9877c28f38
Don Denton <don@happycollision.com>
2018-06-25 14:41:19 -0500

    export necessaries from module

--- src/modules/recorder/recorder.module.ts
+++ src/modules/recorder/recorder.module.ts
@@ -1,10 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecorderService, RecordSettings, RECORD_SETTINGS } from './recorder.service';
+import { VideoRecordingMonitorComponent } from './components/video-recording-monitor/video-recording-monitor.component';
+
+export {
+  VideoRecordingMonitorComponent,
+  RecorderService,
+  RecordSettings,
+};
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [],
+  declarations: [
+    VideoRecordingMonitorComponent,
+  ],
+  exports: [
+    VideoRecordingMonitorComponent,
+  ],
 })
-export class RecorderModule { }
+export class RecorderModule {
+  static forRoot(recordSettings?: RecordSettings): ModuleWithProviders {
+    return {
+      ngModule: RecorderModule,
+      providers: [
+        {provide: RECORD_SETTINGS, useValue: recordSettings},
+        RecorderService,
+      ],
+    };
+  }
+}
