645eab0638328efb7aab1f7db609099735541b4a
Don Denton <don@happycollision.com>
2018-08-22 11:46:10 -0500

    correct typo

--- src/@flixpress/angular-simple-components/modal/modalbase.ts
+++ src/@flixpress/angular-simple-components/modal/modalbase.ts
@@ -11,7 +11,7 @@ export abstract class ModalBase implements OnDestroy, AfterViewInit, OnChanges {
   @Output() onClose: EventEmitter<any> = new EventEmitter<any>();
 
   private _isOpen: boolean;
-  public get shoudDisplay(): boolean { return this._isOpen; }
+  public get shouldDisplay(): boolean { return this._isOpen; }
   classes: object;
 
   ngAfterViewInit() {
